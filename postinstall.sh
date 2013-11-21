1e7f58fc05f14cc98cca93330a55eafd0ebded06
Byron Wong <byronmwong@gmail.com>
2013-11-21 16:15:02 -0500

    grr couldn't make the conditional work

--- postinstall.sh
+++ postinstall.sh
@@ -4,10 +4,5 @@ cp -r node_modules webapp
 cd webapp
 bower install
 
-echo "HAHAHAHAHA"
-echo $BUILD_ENV
-if [[ $BUILD_ENV == "production" ]]; then
-    echo "---------> building assets"
-    grunt dist
-    cp -r dist/assets ../public
-fi
+grunt dist
+cp -r dist/assets ../public

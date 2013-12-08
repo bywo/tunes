7e68e7e47510404e5d0a7d5aba6575708d7bcbac
John Ong <johnmanong@gmail.com>
2013-12-08 13:09:46 -0500

    [jo] pretty print for soundcloud resolver

--- soundcloud_resolve.sh
+++ soundcloud_resolve.sh
@@ -1,1 +1,3 @@
-curl -L 'http://api.soundcloud.com/resolve.json?url='$1'&client_id='$SOUNDCLOUD_KEY
+URL='http://api.soundcloud.com/resolve.json?url='$1'&client_id='$SOUNDCLOUD_KEY
+
+curl -L $URL | ruby -rjson -e 'print JSON.pretty_generate(JSON.parse(STDIN.read))'

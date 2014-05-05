e67bd60f6e386aa559fffc90a959bf3d6c9390d4
Byron Wong <byronmwong@gmail.com>
2014-05-04 20:39:00 -0400

    add helper method to create Song and Sound from SoundCloud url

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,36 @@
 module SongsHelper
+
+  def song_from_soundcloud_url(url)
+    client = HTTPClient.new
+    soundcloud_key = ENV['SOUNDCLOUD_KEY']
+    request_url = "http://api.soundcloud.com/resolve.json?url=#{url}&client_id=#{soundcloud_key}"
+    message = client.get(request_url, :follow_redirect => true)
+    sound_info = JSON.parse(message.content)
+    if sound_info.key? 'errors'
+      fail sound_info['errors'][0]['error_message']
+    elsif !sound_info['streamable']
+      fail 'Sound is not streamable'
+    end
+
+    artist_request_url = "http://api.soundcloud.com/users/#{sound_info['user_id']}.json?client_id=#{soundcloud_key}"
+    artist_message = client.get(artist_request_url)
+    artist_info = JSON.parse(artist_message.content)
+
+    if artist_info.key? 'errors'
+      fail artist_info['errors'][0]['error_message']
+    end
+
+    song = Song.new(:title => sound_info['title'], :artist => artist_info['username'])
+    song.save
+
+    sound = Sound.new({
+      :sound_type => 'soundcloud',
+      :location => sound_info['id'],
+      :song => song
+    })
+    sound.save
+
+    return song
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'spork'
+require 'webmock/rspec'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
@@ -14,7 +15,7 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
 
-  # Requires supporting ruby files with custom matchers and macros, etc, 
+  # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories
   # requires each file in this directory
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -39,8 +40,8 @@ Spork.prefork do
     # of rspec-rails
     config.use_transactional_fixtures = true
 
-    # Run specs in random order to surface order dependencies. If you find an 
-    # order dependency and want to debug it, you can fix the order by providing the 
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing the
     # seed, which is printed after each run
     #   --seed 1234
     config.order = "random"
@@ -82,3 +83,5 @@ end
 #
 # These instructions should self-destruct in 10 seconds.  If they don't, feel
 # free to delete them.
+
+WebMock.disable_net_connect!(allow_localhost: true)

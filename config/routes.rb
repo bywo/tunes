132b36836ec15a4de64b7c1dd69a4cfa0e51de4e
johnmanong <johnmanong@gmail.com>
2014-05-05 20:50:45 -0400

    [jo] clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,4 @@ Tunes::Application.routes.draw do
   resources :memberships
   resources :users
 
-  # if Rails.env.production?
-  #   root 'webapp#index'
-  # end
-
-  # namespace :api do
-  #   resources :songs
-  #   resources :sounds
-  #   resources :keys
-  # end
-
-  # if Rails.env.production?
-  #   get '*path' => 'webapp#index'
-  # end
-
 end
